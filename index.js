5ea13b8b1dfd40057003a72e2b452a2fc2314995
shiyingjie <shiyingjie@rr.tv>
2022-08-07 00:47:58 +0800

    upgrad

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const instance = axios.create({
     headers: {
         origin: 'https://m.18art.art',
         'content-type': 'application/json;charset=UTF-8',
-        'authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjE0MjM0OCwiVXNlckluZm8iOiJWUmZta2EvMituK3I2S2FkN3hqdGFzSlk4YVcvRUJaQlpXand4cmFRTGRFcE9oc0hIeXl2N0R4S0JtaExlVzVQMFZPVDZtQUJqbTQwM0EyaVNjRTRSOTNrTnpQS2g3dE5lNEkrcmR1eFN5a2RvZEkrQ1d0cE5SdnpLV3FDQmJhNm5qV2VQakx2QTZGaE00Z0tZenRpc1E9PSIsIm5iZiI6MTY1OTgwMTcxMywiZXhwIjoxNjYwMjMzNzEzLCJpYXQiOjE2NTk4MDE3MTMsImlzcyI6InNoaWJhX2FkbWluIiwiYXVkIjoic2hpYmFfYWRtaW4ifQ.Z882QFDh9tDbDtfDFUq9K4McZXz82GBc8RXeRfsdlgQ"
+        'authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjQ0ODEwLCJVc2VySW5mbyI6IlRJNHl2Wm1jSzg3a2gzMVZqOHh4TXhCZ0c0a3dqWEltVU51Rk0xcmRDSjFnYkxiZWxlV3FTUk0rVitwR1JSZUcyRmtSa0tac3Q4QkRpdzNpZEZMY2lMaDJGaTdMSklHSnE2bEc0bTk1dkpiWHFHaXBSRDMraVFNYlp3dm9rMUN0Um5wWE4wcDVlRGhacE9NSzRsS0JTZz09IiwibmJmIjoxNjU5ODAzMTY5LCJleHAiOjE2NjAyMzUxNjksImlhdCI6MTY1OTgwMzE2OSwiaXNzIjoic2hpYmFfYWRtaW4iLCJhdWQiOiJzaGliYV9hZG1pbiJ9.XCZ9pMgKyE3NOMwDjwX46scJNdCEMQDfj6DUvKNGq3g"
     }
 });
 
-const PRICE = 4000
+const PRICE = 3900
+
+const groupId = {
+    "215": '老鼠',
+    "213": '山林虎',
+    "217": '牛',
+    "220": '鸡',
+    '201': '猴子',
+    '224': '蛇'
+}
 
 function requestList() {
     console.log('请求列表。。。')
@@ -19,22 +28,22 @@ function requestList() {
         pageIndex: 1,
         pageSize: 10,
     }).then(res => {
-        // console.log(res.data.data)
         let { items } = res.data.data
-        let open = items.filter(item => item.saleStatus == 3 && item.price <= PRICE).map(item => item.id)
-        // console.log(open)
+        let _price = []
+        let open = items.filter(item => item.saleStatus == 3 && item.price <= PRICE).map(item => {
+            _price.push(item.price)
+            return item.id
+        })
         console.log('未匹配到合适的价格。。。')
         if (!open.length) {
             requestList()
             return
         }
-        console.log(open)
-        console.log('requestList::成功')
-        open.forEach(element => {
-            pay(element)
+        // console.log('requestList::成功')
+        open.forEach((id, index) => {
+            pay(id, _price[index])
         });
-    }).catch(err => {
-        // console.log(err)
+    }).catch(() => {
         console.log('requestList::失败')
         requestList()
     })
@@ -44,8 +53,8 @@ function requestList() {
 requestList()
 
 
-function pay(id) {
-    console.log('请求支付。。。')
+function pay(id, _price) {
+    console.log(`请求支付。。。,当前订单价格:${_price}`)
     instance.post('wagg/order/buy', {
         addressId: 0,
         commodityId: id,
@@ -56,10 +65,11 @@ function pay(id) {
         if (!res.data.data || res.data.message != '已拥有一笔待支付订单，请先支付或取消') {
             console.log('支付失败，重新请求列表')
             requestList()
+            return
         }
-        console.log('pay::成功')
-    }).catch(err => {
+        console.log(`pay::成功,当前订单价格:${_price}`)
+        return
+    }).catch(() => {
         console.log('pay::失败')
     })
-
 }

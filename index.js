ee8d9d4ed7b4d504501669aa8c91267f79f8b12a
shiyingjie <shiyingjie@rr.tv>
2022-08-07 11:45:47 +0800

    init

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const axios = require('axios')
+
+var open = require("open");
+
+//列表用的
 const instance = axios.create({
     baseURL: 'https://m.18art.art/api/',
-    timeout: 10000,
+    timeout: 15000,
+    headers: {
+        origin: 'https://m.18art.art',
+        'content-type': 'application/json;charset=UTF-8',
+        'authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjQ0ODEwLCJVc2VySW5mbyI6IlRJNHl2Wm1jSzg3a2gzMVZqOHh4TXhCZ0c0a3dqWEltVU51Rk0xcmRDSjFnYkxiZWxlV3FTUk0rVitwR1JSZUcyRmtSa0tac3Q4QkRpdzNpZEZMY2lMaDJGaTdMSklHSnE2bEc0bTk1dkpiWHFHaXBSRDMraVFNYlp3dm9rMUN0Um5wWE4wcDVlRGhacE9NSzRsS0JTZz09IiwibmJmIjoxNjU5Nzk2OTI0LCJleHAiOjE2NjAyMjg5MjQsImlhdCI6MTY1OTc5NjkyNCwiaXNzIjoic2hpYmFfYWRtaW4iLCJhdWQiOiJzaGliYV9hZG1pbiJ9.S-6trHWxkF6iZRIDgrAwepSP3LuaeGszAjSuLmymXjo"
+    }
+});
+
+//支付用的
+const instance2 = axios.create({
+    baseURL: 'https://m.18art.art/api/',
+    timeout: 15000,
     headers: {
         origin: 'https://m.18art.art',
         'content-type': 'application/json;charset=UTF-8',
@@ -9,7 +24,7 @@ const instance = axios.create({
     }
 });
 
-const PRICE = 3900
+const PRICE = 3300
 
 const groupId = {
     "215": '老鼠',
@@ -43,7 +58,7 @@ function requestList() {
         open.forEach((id, index) => {
             pay(id, _price[index])
         });
-    }).catch(() => {
+    }).catch((e) => {
         console.log('requestList::失败')
         requestList()
     })
@@ -55,10 +70,10 @@ requestList()
 
 function pay(id, _price) {
     console.log(`请求支付。。。,当前订单价格:${_price}`)
-    instance.post('wagg/order/buy', {
+    instance2.post('wagg/order/buy', {
         addressId: 0,
         commodityId: id,
-        payType: 1,
+        payType: 1,//1:银行卡 2:钱包
     }).then(res => {
         console.log(res.data)
         console.log(res.data.data)
@@ -68,6 +83,8 @@ function pay(id, _price) {
             return
         }
         console.log(`pay::成功,当前订单价格:${_price}`)
+        open("http://www.google.com?快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付快去支付", "chrome");
+
         return
     }).catch(() => {
         console.log('pay::失败')

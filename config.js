751b20090497f3f3e0e1a4ce423642a09c8b3629
shiyingjie <shiyingjie@rr.tv>
2022-08-11 14:33:13 +0800

    init

--- config.js
+++ config.js
@@ -4,17 +4,47 @@ const groupId = {
     "217": '牛',
     "220": '鸡',
     '201': '猴子',
-    '224': '蛇'
+    '224': '蛇',
+    '231': '欢喜罗汉',
+    '232': '静坐罗汉'
 }
+const userAgents = [
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0) ,Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre',
+    'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
+    'Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)',
+    'Opera/9.25 (Windows NT 5.1; U; en), Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+]
+
+const lToken = [
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjIyNTg1OSwiVXNlckluZm8iOiJhYnlrald2MUFlSVA1SUxzNS84MzdLOVlkRmd5R0JrNUdxS3piN3VEWCtaclJpeXRPdVBuamVZUVB2TmJ2RXdVYXhybUJwTnNjVVJKL3p5eGJSWWNXU2l4VUpaMkJYVVMxOWtZZ2pyV2dCaGFxeXk1REpKZWJOVE90TFZ0aVMxQUdNQWloNTlUbUcxSHpNMjhLa0sxeVE9PSIsIm5iZiI6MTY2MDA5NjgxMCwiZXhwIjoxNjYwMzEyODEwLCJpYXQiOjE2NjAwOTY4MTAsImlzcyI6InNoaWJhX2FkbWluIiwiYXVkIjoic2hpYmFfYWRtaW4ifQ.8UFCFARkDJqloWJx0WxDHF3iRnkEjUYUNg6ddMo4mtE",
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjIyNTg1OSwiVXNlckluZm8iOiJhYnlrald2MUFlSVA1SUxzNS84MzdLOVlkRmd5R0JrNUdxS3piN3VEWCtaclJpeXRPdVBuamVZUVB2TmJ2RXdVYXhybUJwTnNjVVJKL3p5eGJSWWNXU2l4VUpaMkJYVVMxOWtZZ2pyV2dCaGFxeXk1REpKZWJOVE90TFZ0aVMxQUdNQWloNTlUbUcxSHpNMjhLa0sxeVE9PSIsIm5iZiI6MTY2MDE4MzQ4MywiZXhwIjoxNjYwMzk5NDgzLCJpYXQiOjE2NjAxODM0ODMsImlzcyI6InNoaWJhX2FkbWluIiwiYXVkIjoic2hpYmFfYWRtaW4ifQ.ZH6sgmRRiPBpQV14Mtw25qFEjrYlFcLk0Kq_a-lr9iI',//153
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjIyNTgwNiwiVXNlckluZm8iOiJKUzE2c0RMck44RUFHU2tya2ZIQmd6V2pHVjdTSGhmS3JYTU5VRVI1bVdPd1RDZWc2MDNHWDF1bjVHL05FcUNLOGgySENIQ1VncjRaWGFGaU9CWXpYSDNlbEZuR09JK1BjR2Z4U0lDUWtsL3A3eTlvaEpUWDBIWTFXV3JtSnNiek43VXF4bk9YV09PaUdkSFpBZ005Q2c9PSIsIm5iZiI6MTY2MDE4MzA2NSwiZXhwIjoxNjYwMzk5MDY1LCJpYXQiOjE2NjAxODMwNjUsImlzcyI6InNoaWJhX2FkbWluIiwiYXVkIjoic2hpYmFfYWRtaW4ifQ.ClP1_lDJwNLNktO7Vfn_xfV1gJIO8FNEKjs8ULRHtPo"//173
+]
 const configs = {
+    userAgents,
     groupId,
     //列表token 173
-    LTOKEN: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjIyNTgwNiwiVXNlckluZm8iOiJKUzE2c0RMck44RUFHU2tya2ZIQmd6V2pHVjdTSGhmS3JYTU5VRVI1bVdPd1RDZWc2MDNHWDF1bjVHL05FcUNLOGgySENIQ1VncjRaWGFGaU9CWXpYSDNlbEZuR09JK1BjR2Z4U0lDUWtsL3A3eTlvaEpUWDBIWTFXV3JtSnNiek43VXF4bk9YV09PaUdkSFpBZ005Q2c9PSIsIm5iZiI6MTY2MDA5NjYyOSwiZXhwIjoxNjYwMzEyNjI5LCJpYXQiOjE2NjAwOTY2MjksImlzcyI6InNoaWJhX2FkbWluIiwiYXVkIjoic2hpYmFfYWRtaW4ifQ.Er6GgL9I1l409B4rJLjANLENC80ur_0TVQsYNLmdpsY",
+    LTOKEN: lToken,
     timeout: 15000,
-    timeOut: 1500,//接口延迟调用
+    timeOut: 1000,//接口延迟调用
     PAYTYPE: 1,//1:银行卡 2:钱包
     //支付token
-    TOKEN: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjQ0ODEwLCJVc2VySW5mbyI6IlRJNHl2Wm1jSzg3a2gzMVZqOHh4TXhCZ0c0a3dqWEltVU51Rk0xcmRDSjFnYkxiZWxlV3FTUk0rVitwR1JSZUcyRmtSa0tac3Q4QkRpdzNpZEZMY2lMaDJGaTdMSklHSnE2bEc0bTk1dkpiWHFHaXBSRDMraVFNYlp3dm9rMUN0Um5wWE4wcDVlRGhacE9NSzRsS0JTZz09IiwibmJmIjoxNjU5OTU2NTI1LCJleHAiOjE2NjAzODg1MjUsImlhdCI6MTY1OTk1NjUyNSwiaXNzIjoic2hpYmFfYWRtaW4iLCJhdWQiOiJzaGliYV9hZG1pbiJ9.zja4Cl2PwJT3Yr3B4VD7Gla5fWULM7UKjcY1wqrjfDM",
+    TOKEN: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjQ0ODEwLCJVc2VySW5mbyI6IlRJNHl2Wm1jSzg3a2gzMVZqOHh4TXhCZ0c0a3dqWEltVU51Rk0xcmRDSjFnYkxiZWxlV3FTUk0rVitwR1JSZUcyRmtSa0tac3Q4QkRpdzNpZEZMY2lMaDJGaTdMSklHSnE2bEc0bTk1dkpiWHFHaXBSRDMraVFNYlp3dm9rMUN0Um5wWE4wcDVlRGhacE9NSzRsS0JTZz09IiwibmJmIjoxNjYwMTA2ODkyLCJleHAiOjE2NjA1Mzg4OTIsImlhdCI6MTY2MDEwNjg5MiwiaXNzIjoic2hpYmFfYWRtaW4iLCJhdWQiOiJzaGliYV9hZG1pbiJ9.9JQKxmaQMpzS9a3QsKUtmVhwz-Q0n8ZGSGN4YDrh3uk",
     // ID: 213, //商品id
     // PRICE: 13000 //商品价格
     ID: process.env.id, //商品id
--- index1.js
+++ index1.js
@@ -5,6 +5,8 @@ const HttpsProxyAgent = require("https-proxy-agent");
 const HttpProxyAgent = require("http-proxy-agent");
 let total = 0
 let page = 1
+let serialLock = true
+let serial = ['5', '6']
 
 
 let { LTOKEN, PRICE, ID, TOKEN, PAYTYPE, timeout, groupId, timeOut, userAgents } = configs
@@ -43,7 +45,7 @@ const instance2 = axios.create({
 
 let timer = null
 function requestList() {
-    console.log(`第---${++total}次----请求列表。。。`)
+    console.log(`第${++total}次--第${page}页--请求列表。。。`)
     instance.defaults.headers['authorization'] = LTOKEN[parseInt(Math.random() * LTOKEN.length)]
     instance.defaults.headers['user-agent'] = userAgents[parseInt(Math.random() * userAgents.length)]
     instance.post('agg/market/commodity-items', {
@@ -62,23 +64,40 @@ function requestList() {
         }
         let { items } = res.data.data
         let _price = []
+        let _commodityNo = []
         let p = String(items.map(item => item.price))
         console.log(`当前-----${groupId[ID]}-----价格：`)
         console.log(`期望价格:${PRICE}`)
+        console.log(`期望编号:${serial}`)
         console.log(p)
-        let open = items.filter(item => item.saleStatus == 3 && item.price <= PRICE).map(item => {
-            _price.push(item.price)
-            return item.id
-        })
-        console.log('未匹配到合适的价格。。。')
+        let open = []
+        if (serialLock) {
+            page++
+            open = items.filter(item => {
+                return item.saleStatus == 3 && serial.includes(item.commodityNo.slice(-1)) && item.price <= PRICE
+            }).map(item => {
+                _price.push(item.price)
+                _commodityNo.push(`#${item.commodityNo}`)
+                return item.id
+            })
+        }
+        else {
+            open = items.filter(item => item.saleStatus == 3 && item.price <= PRICE).map(item => {
+                _price.push(item.price)
+                return item.id
+            })
+        }
+
         if (!open.length) {
+            console.log('未匹配到合适的价格。。。')
             timer = setTimeout(() => {
                 requestList()
             }, timeOut)
             return
         }
         open.forEach((id, index) => {
-            pay(id, _price[index])
+
+            pay(id, _price[index], _commodityNo[index])
         });
     }).catch((e) => {
         console.log(e.code);
@@ -92,8 +111,14 @@ function requestList() {
 
 requestList()
 
-function pay(id, _price) {
+function pay(id, _price, _commodityNo) {
     console.log(`请求支付。。。,当前订单价格:${_price}`)
+    if (serialLock) {
+        console.log(
+            `支付编号${_commodityNo}`
+        );
+    }
+
     instance2.post('wagg/order/buy', {
         addressId: 0,
         commodityId: id,
